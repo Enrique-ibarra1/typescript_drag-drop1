44900c4f5a403f82ac6c7862154de9a7ed67054e
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-04-16 11:48:54 -0500

    added .js extensions to a few imports i missed, whoops

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,27 +1,42 @@
+// Component Base Class
 
-//Component base class
-//<generics, t & u are identifiers>
+export const something = '...';
+
 export default abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement;
     hostElement: T;
     element: U;
 
-    constructor(templateId: string, hostElementId: string, instertAtStart: boolean, newElementId?: string) {
-        //selection the project-list <template>
-        this.templateElement = <HTMLTemplateElement>document.getElementById(templateId)!;
-        //host element is still the app <div>
+    constructor(
+        templateId: string,
+        hostElementId: string,
+        insertAtStart: boolean,
+        newElementId?: string
+    ) {
+        this.templateElement = document.getElementById(
+            templateId
+        )! as HTMLTemplateElement;
         this.hostElement = document.getElementById(hostElementId)! as T;
-        const importedNode = document.importNode(this.templateElement.content, true);
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
         this.element = importedNode.firstElementChild as U;
-        //this elements id will be either 'active' or 'Finished'
         if (newElementId) {
-            this.element.id = newElementId
+            this.element.id = newElementId;
         }
-        this.attach(instertAtStart);
+
+        this.attach(insertAtStart);
     }
+
     private attach(insertAtBeginning: boolean) {
-        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element)
-    };
-    abstract configure?(): void;
+        this.hostElement.insertAdjacentElement(
+            insertAtBeginning ? 'afterbegin' : 'beforeend',
+            this.element
+        );
+    }
+
+    abstract configure(): void;
     abstract renderContent(): void;
 }
